fd8641e24e365c21d2490f633b584c2a794e90ad
aitoolshub <kidsnurseryrhymes0@gmail.com>
2026-08-05 16:45:47 +0500

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -6,10 +6,63 @@ export default async function handler(req, res) {
         });
     }
 
-    // Yahan user ka message receive hoga
+    try {
 
-    // Yahan Groq API call hogi
+        const { message } = req.body;
 
-    // AI ka response website ko return hoga
+        if (!message) {
+            return res.status(400).json({
+                error: "Message required"
+            });
+        }
+
+        const response = await fetch(
+            "https://api.groq.com/openai/v1/chat/completions",
+            {
+                method: "POST",
+                headers: {
+                    "Authorization": `Bearer ${process.env.GROQ_API_KEY}`,
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+
+                    model: "llama-3.3-70b-versatile",
+
+                    messages: [
+                        {
+                            role: "system",
+                            content: "You are a helpful AI assistant."
+                        },
+                        {
+                            role: "user",
+                            content: message
+                        }
+                    ]
+
+                })
+            }
+        );
+
+
+        const data = await response.json();
+
+
+        res.status(200).json({
+
+            answer:
+            data.choices[0].message.content
+
+        });
+
+
+    } catch(error){
+
+        res.status(500).json({
+
+            error: error.message
+
+        });
+
+    }
 
 }
