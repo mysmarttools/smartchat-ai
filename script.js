c5c057748766e6003679f3e6c82979af02bb13c9
aitoolshub <kidsnurseryrhymes0@gmail.com>
2026-08-05 16:24:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
+const input = document.getElementById("userInput");
+const sendBtn = document.getElementById("sendBtn");
+const messages = document.getElementById("messages");
 
+// Send Button
+sendBtn.addEventListener("click", sendMessage);
+
+// Press Enter
+input.addEventListener("keydown", function (e) {
+    if (e.key === "Enter" && !e.shiftKey) {
+        e.preventDefault();
+        sendMessage();
+    }
+});
+
+function sendMessage() {
+
+    const text = input.value.trim();
+
+    if (text === "") return;
+
+    // User Message
+    addMessage(text, "user");
+
+    input.value = "";
+
+    // Fake AI Thinking
+    setTimeout(() => {
+
+        addMessage("🤖 Thinking...", "ai");
+
+    }, 500);
+
+}
+
+// Add Message
+function addMessage(text, type) {
+
+    const message = document.createElement("div");
+
+    message.classList.add("message");
+
+    if (type === "user") {
+        message.classList.add("user-message");
+    } else {
+        message.classList.add("ai-message");
+    }
+
+    message.innerHTML = `<p>${text}</p>`;
+
+    messages.appendChild(message);
+
+    messages.scrollTop = messages.scrollHeight;
+
+}
